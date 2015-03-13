559e9b4c583cc4e3e466013c7d47a98e79244044
Arata Miyamoto <arata@media.mit.edu>
2015-03-13 17:57:33 +0000

    show LiveObjects with RoundedImageView
    git-svn-id: https://svn.media.mit.edu/r/shair/projects/LiveObjects/branches/tim_design_porting@2101 d7055ea3-8c51-4d62-930f-d8676c13f5f0

--- app/src/main/java/edu/mit/media/obm/liveobjects/app/widget/RoundedImageView.java
+++ app/src/main/java/edu/mit/media/obm/liveobjects/app/widget/RoundedImageView.java
@@ -10,6 +10,7 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
 import android.widget.ImageView;
 
 /**
@@ -18,6 +19,14 @@ import android.widget.ImageView;
 public class RoundedImageView extends ImageView {
     public RoundedImageView(Context context) {
         super(context);
+    }
+
+    public RoundedImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundedImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
     }
 
     @Override
