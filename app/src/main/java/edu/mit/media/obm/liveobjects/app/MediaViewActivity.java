67d36b274d5738918e4a7020aa475343b1767cfa
Arata Miyamoto <arata@media.mit.edu>
2015-03-19 02:38:36 +0000

    use no animation when open the detail view
    git-svn-id: https://svn.media.mit.edu/r/shair/projects/LiveObjects/branches/tim_design_porting2@2144 d7055ea3-8c51-4d62-930f-d8676c13f5f0

--- app/src/main/java/edu/mit/media/obm/liveobjects/app/MediaViewActivity.java
+++ app/src/main/java/edu/mit/media/obm/liveobjects/app/MediaViewActivity.java
@@ -5,6 +5,8 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import edu.mit.media.obm.liveobjects.storage.wifi.WifiStorageConfig;
 import edu.mit.media.obm.shair.liveobjects.R;
@@ -92,5 +94,32 @@ public class MediaViewActivity extends ActionBarActivity implements OnMediaViewL
     @Override
     public boolean onInfo(MediaPlayer mp, int what, int extra) {
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_goto_home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(intent);
+            finish();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
