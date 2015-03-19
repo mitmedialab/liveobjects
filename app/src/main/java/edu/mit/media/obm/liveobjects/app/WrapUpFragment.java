96f7b9baf7494e86106f6cee655a52015af51ccf
Arata Miyamoto <arata@media.mit.edu>
2015-03-19 23:00:30 +0000

    show main background in the wrap up page as a placeholder
    git-svn-id: https://svn.media.mit.edu/r/shair/projects/LiveObjects/trunk@2151 d7055ea3-8c51-4d62-930f-d8676c13f5f0

--- app/src/main/java/edu/mit/media/obm/liveobjects/app/WrapUpFragment.java
+++ app/src/main/java/edu/mit/media/obm/liveobjects/app/WrapUpFragment.java
@@ -1,12 +1,18 @@
 package edu.mit.media.obm.liveobjects.app;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import edu.mit.media.obm.liveobjects.app.widget.BitmapEditor;
 import edu.mit.media.obm.shair.liveobjects.R;
 
 /**
@@ -41,6 +47,14 @@ public class WrapUpFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_wrap_up, container, false);
 
+        Bitmap background = BitmapFactory.decodeResource(getResources(), R.drawable.main_background);
+        BitmapEditor bitmapEditor = new BitmapEditor(getActivity());
+        background = bitmapEditor.cropToDisplayAspectRatio(background, getActivity().getWindowManager());
+        bitmapEditor.blurBitmap(background, 2);
+
+        BitmapDrawable drawableBackground = new BitmapDrawable(getResources(), background);
+        RelativeLayout rootLayout = (RelativeLayout) rootView.findViewById(R.id.root_layout);
+        rootLayout.setBackground(drawableBackground);
 
         return rootView;
     }
