3239e54a8c77bcb19045d134b2448c58dda64555
Arata Miyamoto <arata@media.mit.edu>
2015-04-03 20:14:25 +0000

    format WifiManagerWrapper.java
    git-svn-id: https://svn.media.mit.edu/r/shair/projects/LiveObjects/trunk@2201 d7055ea3-8c51-4d62-930f-d8676c13f5f0

--- liveobjects_network_wifi_mod/src/main/java/edu/mit/media/obm/liveobjects/driver/wifi/WifiManagerWrapper.java
+++ liveobjects_network_wifi_mod/src/main/java/edu/mit/media/obm/liveobjects/driver/wifi/WifiManagerWrapper.java
@@ -62,45 +62,45 @@ public class WifiManagerWrapper {
 
     /**
      * Connect to a configured network.
+     *
      * @param wifiManager
      * @param config
      * @param numOpenNetworksKept Settings.Secure.WIFI_NUM_OPEN_NETWORKS_KEPT
      * @return
      */
-    public static boolean connectToConfiguredNetwork(final Context ctx, final WifiManager wifiMgr, WifiConfiguration config, boolean reassociate)
-    {
+    public static boolean connectToConfiguredNetwork(final Context ctx, final WifiManager wifiMgr, WifiConfiguration config, boolean reassociate) {
         int oldPri = config.priority;
         // Make it the highest priority.
         int newPri = getMaxPriority(wifiMgr) + 1;
-        if(newPri > MAX_PRIORITY) {
+        if (newPri > MAX_PRIORITY) {
             newPri = shiftPriorityAndSave(wifiMgr);
             config = getWifiConfiguration(wifiMgr, config);
-            if(config == null) {
+            if (config == null) {
                 return false;
             }
         }
-        
+
         // Set highest priority to this configured network
         config.priority = newPri;
         int networkId = wifiMgr.updateNetwork(config);
-        if(networkId == -1) {
+        if (networkId == -1) {
             return false;
         }
-        
+
         // Do not disable others
-        if(!wifiMgr.enableNetwork(networkId, false)) {
+        if (!wifiMgr.enableNetwork(networkId, false)) {
             config.priority = oldPri;
             return false;
         }
-        
-        if(!wifiMgr.saveConfiguration()) {
+
+        if (!wifiMgr.saveConfiguration()) {
             config.priority = oldPri;
             return false;
         }
-        
+
         // We have to retrieve the WifiConfiguration after save.
         config = getWifiConfiguration(wifiMgr, config);
-        if(config == null) {
+        if (config == null) {
             return false;
         }
         //
@@ -115,37 +115,37 @@ public class WifiManagerWrapper {
         }
         */
         //TODO: verify if to disable others is needed if not change second param to false
-        if(!wifiMgr.enableNetwork(config.networkId, true)) {
+        if (!wifiMgr.enableNetwork(config.networkId, true)) {
             return false;
         }
-        
+
         //final boolean connect = reassociate ? wifiMgr.reassociate() : wifiMgr.reconnect();
         final boolean connect = wifiMgr.reconnect();
-        if(!connect) {
+        if (!connect) {
             return false;
         }
-        
+
         return true;
     }
-    
+
     private static void sortByPriority(final List<WifiConfiguration> configurations) {
         java.util.Collections.sort(configurations, new Comparator<WifiConfiguration>() {
 
             @Override
             public int compare(WifiConfiguration object1,
-                    WifiConfiguration object2) {
+                               WifiConfiguration object2) {
                 return object1.priority - object2.priority;
             }
         });
     }
-    
+
     private static final int MAX_PRIORITY = 99999;
-    
+
     private static int shiftPriorityAndSave(final WifiManager wifiMgr) {
         final List<WifiConfiguration> configurations = wifiMgr.getConfiguredNetworks();
         sortByPriority(configurations);
         final int size = configurations.size();
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             final WifiConfiguration config = configurations.get(i);
             config.priority = i;
             wifiMgr.updateNetwork(config);
@@ -157,16 +157,15 @@ public class WifiManagerWrapper {
     private static int getMaxPriority(final WifiManager wifiManager) {
         final List<WifiConfiguration> configurations = wifiManager.getConfiguredNetworks();
         int pri = 0;
-        for(final WifiConfiguration config : configurations) {
-            if(config.priority > pri) {
+        for (final WifiConfiguration config : configurations) {
+            if (config.priority > pri) {
                 pri = config.priority;
             }
         }
         return pri;
     }
 
-    public static WifiConfiguration getWifiConfigurationBySsid(final WifiManager wifiMgr, String ssid)
-    {
+    public static WifiConfiguration getWifiConfigurationBySsid(final WifiManager wifiMgr, String ssid) {
         ssid = convertToQuotedString(ssid);
         WifiConfiguration config = null;
         boolean found = false;
@@ -174,7 +173,7 @@ public class WifiManagerWrapper {
         INFO("getWifiConfigurationBySsid");
 
         List<WifiConfiguration> configs = wifiMgr.getConfiguredNetworks();
-        
+
         if (configs == null)
             return null;
 
@@ -185,24 +184,23 @@ public class WifiManagerWrapper {
         while (it.hasNext() && !found) {
             //for (Iterator<WifiConfiguration> it = configs.iterator(); it.hasNext() && !found;) {
             WifiConfiguration configTemp = it.next();
-            INFO( "config " + configTemp.networkId + " " + configTemp.SSID);
+            INFO("config " + configTemp.networkId + " " + configTemp.SSID);
             if (configTemp.SSID.equals(ssid)) {
                 found = true;
                 config = configTemp;
             }
         }
-        
+
         return config;
     }
-    
 
-    public static WifiConfiguration getWifiConfigurationByNetworkId(final WifiManager wifiMgr, final int netid)
-    {
+
+    public static WifiConfiguration getWifiConfigurationByNetworkId(final WifiManager wifiMgr, final int netid) {
         WifiConfiguration config = null;
         boolean found = false;
         List<WifiConfiguration> configs = wifiMgr.getConfiguredNetworks();
 
-        for(Iterator<WifiConfiguration> it=configs.iterator();it.hasNext() && !found;){
+        for (Iterator<WifiConfiguration> it = configs.iterator(); it.hasNext() && !found; ) {
             WifiConfiguration test = it.next();
             if (test.networkId == netid) {
                 found = true;
@@ -213,12 +211,11 @@ public class WifiManagerWrapper {
         return config;
     }
 
-    public static WifiConfiguration getWifiConfiguration(final WifiManager wifiMgr, final WifiConfiguration configToFind)
-    {
+    public static WifiConfiguration getWifiConfiguration(final WifiManager wifiMgr, final WifiConfiguration configToFind) {
         final String ssid = configToFind.SSID;
         if (ssid.length() == 0)
             return null;
-        
+
         final String bssid = configToFind.BSSID;
 
         final List<WifiConfiguration> configurations = wifiMgr.getConfiguredNetworks();
@@ -233,35 +230,31 @@ public class WifiManagerWrapper {
         }
         return null;
     }
-    
-    public static String convertToQuotedString(String string)
-    {
+
+    public static String convertToQuotedString(String string) {
         if (TextUtils.isEmpty(string)) {
             return "";
         }
-        
+
         final int lastPos = string.length() - 1;
         if (lastPos < 0 || (string.charAt(0) == '"' && string.charAt(lastPos) == '"')) {
             return string;
         }
-        
+
         return "\"" + string + "\"";
     }
 
     private final static String TAG = "Wifiutils:MP2PWifi";
 
-    private static void INFO(String msg)
-    {
+    private static void INFO(String msg) {
         Log.i(TAG, msg);
     }
 
-    private static void WARN(String msg)
-    {
+    private static void WARN(String msg) {
         Log.w(TAG, msg);
     }
 
-    private static void ERROR(String msg)
-    {
+    private static void ERROR(String msg) {
         Log.e(TAG, msg);
     }
 
