70f15bf8aaa61bf3f3e76de72377fae0984fffa0
Arata Miyamoto <arata@example.com>
2015-08-24 15:59:00 -0400

    remove codes using android's default logger

--- apptidmarsh/src/main/java/edu/mit/media/obm/liveobjects/apptidmarsh/detail/ContentBrowserFragment.java
+++ apptidmarsh/src/main/java/edu/mit/media/obm/liveobjects/apptidmarsh/detail/ContentBrowserFragment.java
@@ -1,13 +1,9 @@
 package edu.mit.media.obm.liveobjects.apptidmarsh.detail;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
