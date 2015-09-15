462d481b7cd3b38a93ddf062f3b32e5a105f3a10
Arata Miyamoto <artimo14@aratas-mbp.media.mit.edu>
2015-09-15 17:13:57 -0400

    add tets for addActive/SleepingLiveObject()

--- apptidmarsh/src/test/java/edu/mit/media/obm/liveobjects/apptidmarsh/main/DiscoveryInfoTest.java
+++ apptidmarsh/src/test/java/edu/mit/media/obm/liveobjects/apptidmarsh/main/DiscoveryInfoTest.java
@@ -1,10 +1,16 @@
 package edu.mit.media.obm.liveobjects.apptidmarsh.main;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
 import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -12,6 +18,7 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.ObjectGraph;
+import dagger.Provides;
 import edu.mit.media.obm.liveobjects.middleware.common.LiveObject;
 import edu.mit.media.obm.liveobjects.middleware.control.DbController;
 
@@ -22,6 +29,7 @@ import static org.mockito.Mockito.stub;
 /**
  * Created by arata on 9/14/15.
  */
+@RunWith(DataProviderRunner.class)
 public class DiscoveryInfoTest {
     @Inject DiscoveryInfo discoveryInfo;
     @Inject DbController dbController;
@@ -30,7 +38,7 @@ public class DiscoveryInfoTest {
 
     @Module(injects = DiscoveryInfoTest.class)
     class TestModule {
-        @Inject @Singleton
+        @Provides @Singleton
         DbController provideDbController() {
             return mock(DbController.class);
         }
@@ -100,26 +108,89 @@ public class DiscoveryInfoTest {
         assertArrayEquals(expectedLiveObjectList, liveObjectList.toArray());
     }
 
-    @Test
-    public void shouldAddActiveLiveObject() throws Exception {
-        stub(dbController.isLiveObjectEmpty(anyString())).toReturn(true);
-        final int NUM_LIVE_OBJECTS = 4;
-        for (int i = 0; i < NUM_LIVE_OBJECTS; i++) {
-            discoveryInfo.addActiveLiveObject(testLiveObject);
-        }
+    private static class StoredLiveObject {
+        String liveObjectName;
+        boolean connectedBefore;
 
-        List<LiveObject> activeLiveObjectList = discoveryInfo.mActiveLiveObjectList;
-
-        assertEquals(NUM_LIVE_OBJECTS, activeLiveObjectList.size());
-        for (LiveObject liveObject : activeLiveObjectList) {
-            assertEquals("test_name", liveObject.getLiveObjectName());
-            assertEquals(LiveObject.STATUS_ACTIVE, liveObject.getStatus());
-            assertTrue(liveObject.getConnectedBefore());
+        public StoredLiveObject(String liveObjectName, boolean connectedBefore) {
+            this.liveObjectName = liveObjectName;
+            this.connectedBefore = connectedBefore;
         }
     }
 
-    @Test
-    public void testAddSleepingLiveObject() throws Exception {
+    @DataProvider
+    public static Object[][] provideLiveObjectsForAddTests() {
+        return new Object[][] {
+                {
+                        Arrays.asList(
+                                new StoredLiveObject("liveObject01", false))
+                },
+                {
+                        Arrays.asList(
+                                new StoredLiveObject("liveObject01", true))
+                },
+                {
+                        Arrays.asList(
+                                new StoredLiveObject("liveObject01", false),
+                                new StoredLiveObject("liveObject02", false),
+                                new StoredLiveObject("liveObject03", false),
+                                new StoredLiveObject("liveObject04", false))
+                },
+                {
+                        Arrays.asList(
+                                new StoredLiveObject("liveObject01", true),
+                                new StoredLiveObject("liveObject02", true),
+                                new StoredLiveObject("liveObject03", true),
+                                new StoredLiveObject("liveObject04", true))
+                },
+                {
+                        Arrays.asList(
+                                new StoredLiveObject("liveObject01", true),
+                                new StoredLiveObject("liveObject02", false),
+                                new StoredLiveObject("liveObject03", true),
+                                new StoredLiveObject("liveObject04", false))
+                },
+        };
+    }
 
+    private void stubIsLiveObjectEmpty(List<StoredLiveObject> storedLiveObjects) {
+        for (StoredLiveObject storedLiveObject : storedLiveObjects) {
+            String liveObjectName = storedLiveObject.liveObjectName;
+            boolean isConnectedBefore = storedLiveObject.connectedBefore;
+            stub(dbController.isLiveObjectEmpty(liveObjectName)).toReturn(!isConnectedBefore);
+        }
+    }
+
+    private void analyzeAddedLiveObjects(List<StoredLiveObject> storedLiveObjects, List<LiveObject> resultLiveObjects, int expectedStatus) {
+        assertEquals(storedLiveObjects.size(), resultLiveObjects.size());
+        for (int i = 0; i < storedLiveObjects.size(); i++) {
+            assertEquals(storedLiveObjects.get(i).liveObjectName, resultLiveObjects.get(i).getLiveObjectName());
+            assertEquals(expectedStatus, resultLiveObjects.get(i).getStatus());
+            assertEquals(storedLiveObjects.get(i).connectedBefore, resultLiveObjects.get(i).getConnectedBefore());
+        }
+    }
+
+    @Test @UseDataProvider("provideLiveObjectsForAddTests")
+    public void shouldAddActiveLiveObject(List<StoredLiveObject> storedLiveObjects) throws Exception {
+        stubIsLiveObjectEmpty(storedLiveObjects);
+
+        for (StoredLiveObject storedLiveObject : storedLiveObjects) {
+            LiveObject liveObject = new LiveObject(storedLiveObject.liveObjectName);
+            discoveryInfo.addActiveLiveObject(liveObject);
+        }
+
+        analyzeAddedLiveObjects(storedLiveObjects, discoveryInfo.mActiveLiveObjectList, LiveObject.STATUS_ACTIVE);
+    }
+
+    @Test @UseDataProvider("provideLiveObjectsForAddTests")
+    public void shouldAddSleepingObject(List<StoredLiveObject> storedLiveObjects) throws Exception {
+        stubIsLiveObjectEmpty(storedLiveObjects);
+
+        for (StoredLiveObject storedLiveObject : storedLiveObjects) {
+            LiveObject liveObject = new LiveObject(storedLiveObject.liveObjectName);
+            discoveryInfo.addSleepingLiveObject(liveObject);
+        }
+
+        analyzeAddedLiveObjects(storedLiveObjects, discoveryInfo.mSleepingLiveObjectList, LiveObject.STATUS_SLEEPING);
     }
 }
