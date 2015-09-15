403d78105c6c09da6aa0a153af4b47b52062ab7c
Arata Miyamoto <arata@example.com>
2015-09-14 22:52:16 -0400

    add a tes tfor updateLiveObjectList()

--- apptidmarsh/src/test/java/edu/mit/media/obm/liveobjects/apptidmarsh/main/MainFragmentTest.java
+++ apptidmarsh/src/test/java/edu/mit/media/obm/liveobjects/apptidmarsh/main/MainFragmentTest.java
@@ -3,11 +3,16 @@ package edu.mit.media.obm.liveobjects.apptidmarsh.main;
 import com.squareup.otto.Bus;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.powermock.reflect.Whitebox;
 import org.robolectric.RobolectricTestRunner;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -18,6 +23,7 @@ import dagger.Module;
 import dagger.ObjectGraph;
 import dagger.Provides;
 import edu.mit.media.obm.liveobjects.apptidmarsh.utils.LiveObjectNotifier;
+import edu.mit.media.obm.liveobjects.middleware.common.LiveObject;
 import edu.mit.media.obm.liveobjects.middleware.control.ContentController;
 import edu.mit.media.obm.liveobjects.middleware.control.DbController;
 import edu.mit.media.obm.liveobjects.middleware.control.NetworkController;
@@ -76,6 +82,42 @@ public class MainFragmentTest {
 
     @Test
     public void testUpdateDiscoveredLiveObjectList() throws Exception {
+        List<LiveObject> liveObjectList = Whitebox.getInternalState(mainFragment, "mLiveObjectList");
+        List<LiveObject> activeLiveObjectList = Whitebox.getInternalState(mainFragment, "mActiveLiveObjectList");
+        List<LiveObject> sleepingLiveObjectList = Whitebox.getInternalState(mainFragment, "mSleepingLiveObjectList");
+        List<LiveObject> previouslyDetectedLiveObjectList = Whitebox.getInternalState(mainFragment, "mPreviouslyDetectedLiveObjectList");
 
+        LiveObject[] activeLiveObjects = new LiveObject[]{
+                new LiveObject("liveObject01"),
+                new LiveObject("liveObject02"),
+                new LiveObject("liveObject03"),
+                new LiveObject("liveObject04")
+        };
+
+        for (LiveObject liveObject : activeLiveObjects) {
+            liveObject.setStatus(LiveObject.STATUS_ACTIVE);
+            activeLiveObjectList.add(liveObject);
+        }
+
+        LiveObject[] sleepingLiveObjects = new LiveObject[]{
+                new LiveObject("liveObject03"),
+                new LiveObject("liveObject04"),
+                new LiveObject("liveObject05"),
+                new LiveObject("liveObject06")
+        };
+
+        for (LiveObject liveObject : sleepingLiveObjects) {
+            liveObject.setStatus(LiveObject.STATUS_SLEEPING);
+            sleepingLiveObjectList.add(liveObject);
+        }
+
+        Whitebox.invokeMethod(mainFragment, "updateLiveObjectList");
+
+        LiveObject[] expectedLiveObjectList = new LiveObject[] {
+                activeLiveObjects[0], activeLiveObjects[1], activeLiveObjects[2], activeLiveObjects[3],
+                sleepingLiveObjects[2], sleepingLiveObjects[3]
+        };
+
+        Assert.assertArrayEquals(expectedLiveObjectList, liveObjectList.toArray());
     }
 }
